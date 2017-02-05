db0b1d26862ab3ba8841d7aaba3d525308281d90
drfailov <fsoft-p@narod.ru>
2017-02-06 00:08:02 +0200

    Initial commit

---
+++ nokia_5100_r03/console.ino
@@ -0,0 +1,179 @@
+#define WHITE       0  // For drawing pixels. A 0 draws white.
+#define BLACK       1  // A 1 draws black.
+
+int line_height = 8;
+int line_length = 14;
+int current_y = line_height*5;
+int current_x = 0;
+#define LCD_WIDTH   84 // Note: x-coordinates go wide
+#define LCD_HEIGHT  48 // Note: y-coordinates go high
+
+
+
+void print(bool number){
+  if(number)
+    print("true");
+  else  
+    print("false");
+}
+void print(double number){
+  char* buf = new char[6];
+  String(number).toCharArray(buf, 10);
+  print(buf);
+  delete buf;
+}
+void print(float number){
+  char* buf = new char[6];
+  String(number).toCharArray(buf, 10);
+  print(buf);
+  delete buf;
+}
+void print(int number){
+  char* buf = new char[6];
+  String(number).toCharArray(buf, 6);
+  print(buf);
+  delete buf;
+}
+
+
+
+
+
+void print_animated(bool number){
+  if(number)
+    print_animated("true");
+  else  
+    print_animated("false");
+}
+void print_animated(double number){
+  char* buf = new char[6];
+  String(number).toCharArray(buf, 10);
+  print_animated(buf);
+  delete buf;
+}
+void print_animated(float number){
+  char* buf = new char[6];
+  String(number).toCharArray(buf, 10);
+  print_animated(buf);
+  delete buf;
+}
+void print_animated(int number){
+  char* buf = new char[6];
+  String(number).toCharArray(buf, 6);
+  print_animated(buf);
+  delete buf;
+}
+
+
+
+
+
+void println_animated(bool number){
+  if(number)
+    println_animated("true");
+  else  
+    println_animated("false");
+}
+void println_animated(double number){
+  char* buf = new char[6];
+  String(number).toCharArray(buf, 10);
+  println_animated(buf);
+  delete buf;
+}
+void println_animated(float number){
+  char* buf = new char[6];
+  String(number).toCharArray(buf, 10);
+  println_animated(buf);
+  delete buf;
+}
+void println_animated(int number){
+  char* buf = new char[6];
+  String(number).toCharArray(buf, 6);
+  println_animated(buf);
+  delete buf;
+}
+
+
+
+
+
+
+void println(bool number){
+  if(number)
+    println("true");
+  else  
+    println("false");
+}
+void println(double number){
+  char* buf = new char[6];
+  String(number).toCharArray(buf, 10);
+  println(buf);
+  delete buf;
+}
+void println(float number){
+  char* buf = new char[6];
+  String(number).toCharArray(buf, 10);
+  println(buf);
+  delete buf;
+}
+void println(int number){
+  char* buf = new char[6];
+  String(number).toCharArray(buf, 6);
+  println(buf);
+  delete buf;
+}
+void println(char * dString){
+  print(dString);
+  print("\n");
+}
+
+
+
+void println_animated(char * dString){
+  print_animated(dString);
+  print_animated("\n");
+}
+
+void print(char * dString){
+  bool bw = true;
+  
+  while (*dString != 0x00) // loop until null terminator
+  {
+    //переход на новую строку если символ перехода
+    if(*dString == '\n'){
+      dString++;
+      pull_lines_up();
+      current_x = 0;
+    }
+    else{
+      //нарисовать символ
+      setChar(*dString++, current_x, current_y, bw);
+      current_x+=5;
+  
+      //один пиксель после сивкола
+      for (int i=current_y; i<current_y+8; i++)
+        setPixel(current_x, i, !bw);
+      current_x++;
+  
+      //Переход на новую строку если конец строки
+      if (current_x > (LCD_WIDTH - 5)) {
+        pull_lines_up();
+        current_x = 0;
+      }
+    }
+  }
+  updateDisplay();
+}
+
+void print_animated(char* dString){
+  while (*dString != 0x00){
+    char toPrint[2];
+    char c = *dString;
+    dString++;
+    toPrint[0] = c;
+    toPrint[1] = 0x00;
+    print(toPrint);
+    delay(50);
+  }
+}
+
---
+++ nokia_5100_r03/nokia_5100_r03.ino
@@ -0,0 +1,84 @@
+void setup()
+{
+  lcdBegin(); // This will setup our pins, and initialize the LCD
+  setContrast(40); // Good values range from 40-60
+  clearDisplay(0);
+  updateDisplay();
+  digitalWrite(9, LOW);//BL trigger
+}
+
+
+void loop()
+{
+  println_animated("Hello! Let's check console capabilities:");
+  println("");
+  delay(1000);
+  
+  println_animated("> print()"); delay(500);
+  print("One "); delay(500);
+  print("Two "); delay(500);
+  print("Three"); delay(1000);
+  println(""); println("");
+  
+  println_animated("> println()"); delay(500); 
+  println("One "); delay(500);
+  println("Two "); delay(500);
+  println("Three"); delay(1000);
+  println("");
+
+  println_animated("> print_animated()"); delay(1000);
+  print_animated("One "); delay(500);
+  print_animated("Two "); delay(500);
+  print_animated("Three"); delay(1000);
+  println(""); println("");
+
+  println_animated("> println_animated()"); delay(1000);
+  println_animated("One "); delay(500);
+  println_animated("Two "); delay(500);
+  println_animated("Three"); delay(1000);
+  println(""); println("");
+
+  println_animated("> print int"); delay(1000);
+  print(228); print(" "); delay(500);
+  print(1488); print(" "); delay(500);
+  print(-42); print(" "); delay(500);
+  println("");  println("");
+
+
+  println_animated("> print float"); delay(1000);
+  print(2.88f); print(" "); delay(500);
+  print(1.488f); print(" "); delay(500);
+  println(""); println("");
+
+
+  println_animated("> print double"); delay(1000);
+  print(2.88d); print(" "); delay(500);
+  print(1.488d); print(" "); delay(500);
+  println(""); println("");
+
+
+  println_animated("> print bool"); delay(1000);
+  print(true); print(" "); delay(500);
+  print(false); print(" "); delay(500);
+  println(""); println("");
+
+
+  println_animated("> print all animated"); delay(1000);
+  print_animated(true); print(" "); delay(500);
+  print_animated(1488); print(" "); delay(500);
+  print_animated(14.4f); print(" "); delay(500);
+  print_animated(50.5d); print(" "); delay(500);
+  println(""); println("");
+
+
+  println_animated("> println all animated"); delay(1000);
+  println_animated(true); delay(500);
+  println_animated(1488); delay(500);
+  println_animated(14.4f); delay(500);
+  println_animated(50.5d); delay(500);
+  println(""); println("");
+  
+  
+  println_animated("Make fun!=)"); delay(5000);
+}
+
